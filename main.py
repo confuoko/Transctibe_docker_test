cc186890fb5f49b020c3f964b5e2e86bf12561de
Nataliia <matveeva.github@mail.ru>
2025-04-08 21:25:33 +0300

    first commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from services.transcribe_service import processFile
 
 def main():
-    load_dotenv()
+    #load_dotenv()
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     bucket_name = "whisper-audiotest"
